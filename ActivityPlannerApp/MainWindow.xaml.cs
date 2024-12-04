f1a73ebdd553abae673325d45c8c41d803e6b36b
Thomas Fisher <tom2656@gmail.com>
2024-12-04 21:03:29 +0000

    Initial commit

---
+++ ActivityPlannerApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ActivityPlannerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            Window mainWindow = Application.Current.MainWindow;
+            if (mainWindow.WindowState != WindowState.Maximized)
+                mainWindow.WindowState = WindowState.Maximized;
+            else
+                mainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
