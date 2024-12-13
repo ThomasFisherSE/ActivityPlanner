8b44faa6c60fc648f68807c2194c94aa0691e51b
Thomas Fisher <t.d.fisher.se@gmail.com>
2024-12-13 19:52:39 +0000

    Added DateTimeUtils class

---
+++ ActivityPlannerApp/Core/DateTimeUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace ActivityPlannerApp.Core
+{
+    internal static class DateTimeUtils
+    {
+        public static DateOnly Today => DateOnly.FromDateTime(DateTime.Now);
+
+        public static TimeOnly CurrentTime => TimeOnly.FromDateTime(DateTime.Now);
+    }
+}
