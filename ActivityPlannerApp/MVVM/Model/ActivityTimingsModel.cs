91ab5dbda5612ff2d21f0805f7b09ffad1b4740f
Thomas Fisher <t.d.fisher.se@gmail.com>
2024-12-13 19:56:44 +0000

    Added ActivityTimingsModel
    The idea behind this class is to map activities to time slots e.g. "MyActivity" occurs at 10am on 01/01/25 and 1pm on 01/02/25.

---
+++ ActivityPlannerApp/MVVM/Model/ActivityTimingsModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ActivityPlannerApp.MVVM.Model
+{
+    internal class ActivityTimingsModel
+    {
+        public Dictionary<ActivityModel, List<TimeSlot>> ActivityTimeSlots { get; } = [];
+
+        public void AddActivityToTimeSlot(ActivityModel activity, TimeSlot timeSlot)
+        {
+            if (ActivityTimeSlots.TryGetValue(activity, out List<TimeSlot>? activityTimeSlots)
+                && activityTimeSlots != null)
+            {
+                activityTimeSlots.Add(timeSlot);
+            }
+            else
+            {
+                activityTimeSlots = [timeSlot];
+            }
+
+            ActivityTimeSlots[activity] = activityTimeSlots;
+        }
+
+        public List<TimeSlot> GetTimes(ActivityModel activity)
+        {
+            return ActivityTimeSlots[activity];
+        }
+    }
+}
