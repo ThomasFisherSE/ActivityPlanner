8f263b56bf43a8ecac171161643e465245f5314f
Thomas Fisher <t.d.fisher.se@gmail.com>
2024-12-18 22:42:47 +0000

    Added StringToBitmapImageConverter

---
+++ ActivityPlannerApp/Core/StringToBitmapImageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ActivityPlannerApp.Core
+{
+    public class StringToBitmapImageConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is string path && Uri.TryCreate(path, UriKind.RelativeOrAbsolute, out Uri? imageUri)
+                ? new BitmapImage(imageUri)
+                : null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}
