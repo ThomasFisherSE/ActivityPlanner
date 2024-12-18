c6f6355ff1d955dd862029b56190bd1c8cc28e9c
Thomas Fisher <t.d.fisher.se@gmail.com>
2024-12-18 22:22:25 +0000

    Renamed TimeOnlyConverter to DateTimeToTimeStringConverter

--- ActivityPlannerApp/Core/DateTimeToTimeStringConverter.cs
+++ ActivityPlannerApp/Core/DateTimeToTimeStringConverter.cs
@@ -3,7 +3,7 @@ using System.Windows.Data;
 
 namespace ActivityPlannerApp.Core
 {
-    public class TimeOnlyConverter : IValueConverter
+    public class DateTimeToTimeStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
