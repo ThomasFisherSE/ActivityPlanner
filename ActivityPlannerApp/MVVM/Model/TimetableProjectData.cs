b8409cbdd7cfa67dec340d77b479f3b8270d7f54
Thomas Fisher <t.d.fisher.se@gmail.com>
2024-12-20 00:18:34 +0000

    Added TimetableProjectData struct
    Holds all data for a project.

---
+++ ActivityPlannerApp/MVVM/Model/TimetableProjectData.cs
@@ -0,0 +1,16 @@
+﻿namespace ActivityPlannerApp.MVVM.Model
+{
+    public struct TimetableProjectData
+    {
+        public IList<ActivityModel> Activities { get; set; } = Array.Empty<ActivityModel>();
+
+        public IList<LocationModel> Locations { get; set; } = Array.Empty<LocationModel>();
+
+        public ActivityTimingsModel ActivityTimings { get; set; } = new ActivityTimingsModel();
+
+        public TimetableProjectData()
+        {
+
+        }
+    }
+}
