7513231a96c7adf2366dc3337cc7f4deb2927c58
Thomas Fisher <t.d.fisher.se@gmail.com>
2024-12-20 00:32:45 +0000

    Removed unnecessary usings

--- ActivityPlannerApp/Core/TimetableProjectDataService.cs
+++ ActivityPlannerApp/Core/TimetableProjectDataService.cs
@@ -1,12 +1,7 @@
 ﻿using ActivityPlannerApp.MVVM.Model;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace ActivityPlannerApp.Core
