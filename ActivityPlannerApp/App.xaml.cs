304c990c49d1632d10a6072033f94c8484d332e7
Thomas Fisher <t.d.fisher.se@gmail.com>
2025-01-31 17:40:59 +0000

    Syncfusion license registration based on envvar

--- ActivityPlannerApp/App.xaml.cs
+++ ActivityPlannerApp/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Syncfusion.Licensing;
 using System.Windows;
 
 namespace ActivityPlannerApp
@@ -9,6 +8,21 @@ namespace ActivityPlannerApp
     /// </summary>
     public partial class App : Application
     {
+        private const string SyncfusionLicenseVar = "SYNCFUSION_LICENSE";
+
+        public App()
+        {
+            RegisterSyncfusionLicense();
+        }
+
+        private static void RegisterSyncfusionLicense()
+        {
+            // Use CI to provide license key in published builds, but for developer environment,
+            // use environment variable to determine Syncfusion license key
+            string? syncfusionLicense = Environment.GetEnvironmentVariable(SyncfusionLicenseVar);
+            if (!string.IsNullOrEmpty(syncfusionLicense))
+                SyncfusionLicenseProvider.RegisterLicense(syncfusionLicense);
+        }
     }
 
 }
