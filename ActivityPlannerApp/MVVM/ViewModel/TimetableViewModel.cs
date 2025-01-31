5ae4f1cebf34ef02e6b98ee32b9abf88383628ad
Thomas Fisher <t.d.fisher.se@gmail.com>
2025-01-31 17:44:47 +0000

    Removed unused properties from TimetableViewModel

--- ActivityPlannerApp/MVVM/ViewModel/TimetableViewModel.cs
+++ ActivityPlannerApp/MVVM/ViewModel/TimetableViewModel.cs
@@ -14,14 +14,12 @@ namespace ActivityPlannerApp.MVVM.ViewModel
 
         public int NumberOfDayColumns { get; set; } = 7;
 
-        public IList<DateOnly> ColumnDays { get; }
-
-        public IList<TimeRange> RowTimeRanges { get; }
-
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public TimetableViewModel()
         {
-            ColumnDays = GenerateColumnDays(DateOnly.FromDateTime(StartDateTime), NumberOfDayColumns);
-            RowTimeRanges = GenerateRowTimeRanges(TimeSpanPerCell);
+
         }
 
         public TimetableEntry CreateTimetableEntry(ActivityModel activity, TimeSlot timeSlot)
@@ -42,42 +40,6 @@ namespace ActivityPlannerApp.MVVM.ViewModel
                     CreateTimetableEntry(activity, timeSlot);
                 }
             }
-        }
-
-        public IList<TimeRange> GenerateRowTimeRanges(TimeSpan timeSpan)
-        {
-            IList<TimeRange> timeRanges = [];
-
-            TimeOnly startTime = TimeOnly.MinValue;
-            bool isEndOfDay = false;
-            do
-            {
-                TimeOnly endTime = startTime.Add(timeSpan, out int wrappedDays);
-
-                // Check if the end of the day was reached, in which case, cut the time range short
-                if (wrappedDays > 0)
-                {
-                    endTime = TimeOnly.MaxValue;
-                    isEndOfDay = true;
-                }
-
-                timeRanges.Add(new TimeRange(startTime, endTime));
-
-                // Start the next range at the end of the previous range
-                startTime = endTime;
-            } while (!isEndOfDay);
-
-            return timeRanges;
-        }
-
-        public IList<DateOnly> GenerateColumnDays(DateOnly startDate, int numberOfDays)
-        {
-            IList<DateOnly> days = [];
-            for (int i = 0; i < numberOfDays; i++)
-            {
-                days.Add(startDate.AddDays(i));
-            }
-            return days;
         }
     }
 }
