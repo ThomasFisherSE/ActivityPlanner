47ea3ca9903d02e668384bfbf266204e2f3e9e40
Thomas Fisher <t.d.fisher.se@gmail.com>
2025-02-07 10:45:40 +0000

    Created converter for converting a Color to a SolidColorBrush and vice versa

---
+++ ActivityPlannerApp/Core/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ActivityPlannerApp.Core
+{
+    public class ColorToSolidColorBrushConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                return new SolidColorBrush(color);
+            }
+
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SolidColorBrush solidColorBrush)
+            {
+                return solidColorBrush.Color;
+            }
+
+            return null;
+        }
+    }
+}
